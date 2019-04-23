d7c89c0a669cb4a47817ae64fb235ccc607332a5
Emanuel Muema <emanuel@aims.ac.za>
2019-04-23 16:48:40 +0200

    Correcting miscoding

--- Data_cleaning.R
+++ Data_cleaning.R
@@ -11,7 +11,7 @@ COD1997_2016 <- read_csv(here("COD1997!2016!COD1997!2016_F1.csv"))
 # Correct miscoding of death type -----------------------------------------
 
 COD1997_2016_p95 <- COD1997_2016 %>% 
-  filter(CauseA == "P95", CauseB == "P95",
-         CauseC == "P95", CauseD == "P95") %>% 
-  subset(DeathYear, DeathType)
+  filter(CauseA == "P95" | CauseB == "P95" | CauseC == "P95" | CauseD == "P95") %>% 
+  select(DeathYear, DeathType)
+
 
